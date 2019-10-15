fc1a17b394f2a518214a969177bde31fc85baf4c
Krzysztof <kpiekarz1983@gmail.com>
2019-10-15 17:01:41 +0200

    Added download section

--- src/js/data/pageData.js
+++ src/js/data/pageData.js
@@ -76,5 +76,11 @@ module.exports = {
       paragraph:
         'Enjoy being a student athlete, the recruiting process can be overwhelming, we want to make this a enjoyable experience.'
     }
-  ]
+  ],
+  download: {
+    mainHeading: 'DOWNLOAD APP',
+    subHeading: 'SHOW EVERYONE WHAT YOU CAN DO!',
+    appstoreBtn: 'DOWNLOAD ON THE APP STORE',
+    googleBtn: 'GET IT ON GOOGLE PLAY',
+  }
 };
